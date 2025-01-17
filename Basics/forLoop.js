99aef4ebd7b1b972e482ae8a00c277c0aea2ead1
Bimal Paudel <me.bimalpdl1@gmail.com>
2025-01-17 23:31:41 +0545

    for-in and for-of loops added in forLoop.js

--- Basics/forLoop.js
+++ Basics/forLoop.js
@@ -20,13 +20,50 @@ for(let i = 0, len1 = cars.length; i < len1; i ++){
   console.log(`car ${i}: ${cars[i]}.`);
 }
 
+// for in loop: The JavaScript for in statement loops through the properties of an Object:
+// syntax: for(key in object) {}
+const Person = {name: "Bimal Paudel", age: 23, gender: "Male"};
+let text = "";
+for(let key in Person){  // each iteration returns a key. The key is used to access the value of the key. The value of the key is person[x].
+  text += `${Person[key]} `;
+}
+console.log(text);
+
+// similar to object, it can also be used in array: syntax => for(variable in array){}
+const numbers = [2, 56, 87.5, 59, 6];
+let text1 = "";
+for(let i in numbers){
+  text1 += `${numbers[i]} `;
+}
+console.log(text1);
+
+// Do not use for in over an Array if the index order is important.
+// The index order is implementation-dependent, and array values may not be accessed in the order you expect.
+// It is better to use a for loop, a for of loop, or Array.forEach() when the order is important.
 
 
+// for of loop: The JavaScript for of statement loops through the values of an iterable object.
+// It lets you loop over iterable data structures such as Arrays, Strings, Maps, NodeLists, and more:
+// syntax: for(variable of iterable) {}
+// variable - For every iteration the value of the next property is assigned to the variable. Variable can be declared with const, let, or var.
+// iterable - An object that has iterable properties.
+let text3 = "";
+for (let i of cars) {
+text3 += `${i} `;
+}
+console.log(text3);
 
 
+// looping over a string: 
+let text4 = "Hello threre!";
+let text5 = "";
+for (let i of text4) {
+  text5 += `${i} `;
+}
+console.log(text5);
 
 
-
+// while() and do while() loops are same as in C++.
 
 
 
