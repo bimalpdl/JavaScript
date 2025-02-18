5a60a8f2dc5e9de4cdb4b7aa7579fc68a7703947
Bimal Paudel <me.bimalpdl1@gmail.com>
2025-02-18 23:25:47 +0545

    Added conditionals on Components

---
+++ Basics/playGround/my-react-app/src/components/Conditional.js
@@ -0,0 +1,15 @@
+// TODO: this whole code is broken, fix it
+function Conditional(props) {
+  return (
+
+    if(props.value){
+      <h1>I am conditional and I am true</h1>
+    }
+    else{
+      <h1>I am conditional and I am false</h1>
+    }
+  
+  );
+}
+
+export default Conditional;
