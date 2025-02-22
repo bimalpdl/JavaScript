5a9f40c1e82d38ad413d0f3b803870566c06f2ca
Bimal Paudel <me.bimalpdl1@gmail.com>
2025-02-22 23:52:39 +0545

    Add PropsChild.jsx

--- Basics/playGround/React/react-app/src/App.jsx
+++ Basics/playGround/React/react-app/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import ReactHook from './components/ReactHook';
 import OnClickExpression from './components/OnClickExpression';
+import PropsChild from './components/PropsChild';
 function App() {
   return (
     <>
     {/* <ReactHook /> */}
 <OnClickExpression />
+<PropsChild >    {/* The elements defined inside component body are called Props childredn, in this case h1 and are childern of 'PropsChild' component */}
+  <h1>Hello there, I'm props children</h1>
+  <p>I am props childern too</p>
+</PropsChild>
     </>
   );
 }
---
+++ Basics/playGround/React/react-app/src/components/PropsChild.jsx
@@ -0,0 +1,8 @@
+function PropsChild({children}) {     {/* access props contents as object destructuring */}
+    return (
+        <h1>{children}</h1>
+
+    );
+}
+
+export default PropsChild;
