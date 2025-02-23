49447f49e629e068d8681f75fe8126522e666eab
Bimal Paudel <me.bimalpdl1@gmail.com>
2025-02-23 23:09:55 +0545

    Add React router on App.jsx

--- Basics/playGround/React/react-app/src/App.jsx
+++ Basics/playGround/React/react-app/src/App.jsx
@@ -1,18 +1,26 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactHook from './components/ReactHook';
 import OnClickExpression from './components/OnClickExpression';
 import PropsChild from './components/PropsChild';
 function App() {
-  return (
-    <>
-    {/* <ReactHook /> */}
-<OnClickExpression />
-<PropsChild >    {/* The elements defined inside component body are called Props childredn, in this case h1 and are childern of 'PropsChild' component */}
-  <h1>Hello there, I'm props children</h1>
-  <p>I am props childern too</p>
-</PropsChild>
-    </>
-  );
+	return (
+		<Router>
+<Routes>
+		{/* <ReactHook /> */}
+    <Route path='/' element={<OnClickExpression />} />
+	  <Route path='reacthook' element={<ReactHook />} />
+    <Route path='propschild' element={
+      	
+		<PropsChild name="Bimal">    {/* The elements defined inside component body are called Props childredn, in this case h1 and are childern of 'PropsChild' component */}
+		<h1>Hello there, I'm props children</h1>
+		<p>I am props childern too</p>
+		</PropsChild>
+    }/>
+    </Routes>
+		</Router>
+
+	);
 }
 
 export default App;
--- Basics/playGround/React/react-app/src/components/PropsChild.jsx
+++ Basics/playGround/React/react-app/src/components/PropsChild.jsx
@@ -1,7 +1,9 @@
 function PropsChild({children}) {     {/* access props contents as object destructuring */}
     return (
-        <h1>{children}</h1>
-
+        <>
+        <h1>{children} </h1>
+        {/* <h2>{props.name}</h2> */}
+</>
     );
 }
 
