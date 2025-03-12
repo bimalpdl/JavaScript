e73c14c12155107840603a7087e7829bfb25d179
Bimal Paudel <me.bimalpdl1@gmail.com>
2025-03-12 12:45:08 +0545

    Fixed array updation on UseStateArray.jsx

--- React/react-app/src/components/UseStateArray.jsx
+++ React/react-app/src/components/UseStateArray.jsx
@@ -1,23 +1,32 @@
-// TODO: this code contins bug, fix it
+import { useState } from 'react';
 
-function UseStateArray() {
-    const [items, setItems] = useState([]);
-  
-    const addItem = () => {
-      setItems([...items, `Item ${items.length + 1}`]);
-    };
-  
-    return (
-      <div>
-        <button onClick={addItem}>Add Item</button>
-        <ul>
-          {items.map((item, index) => (
-            <li key={index}>{item}</li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+function ToDoItem({ itemName }) {
+  return <li>{itemName}</li>
 
- export {UseStateArray};
-  
+}
+
+const UseStateArray = () => {
+
+  const [items, setItems] = useState(["Master React", "Learn Node", "Learn Express", "Learn MongoDB"]);
+
+  function addItem() {
+    setItems([...items, `Item ${items.length + 1}`]);
+  };
+  return (
+    <>
+    <h2>Todo App</h2>
+    <ul>
+     { 
+       items.map((item, index) => (<ToDoItem key={index} itemName={item} />)
+      
+      )}     
+
+    </ul>
+    <button onClick={addItem}>Add Item on List.</button>
+      
+    </>
+
+  );
+}
+
+export { UseStateArray };
